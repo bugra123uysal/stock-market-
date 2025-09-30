01648aac3aac9b27eb13233756bafb0311031406
buğra uysal <uysalbugra134@gmail.com>
2025-09-30 21:52:58 +0300

    percentage change, price differences, and estimated price calculated

--- abd_borsaa.py
+++ abd_borsaa.py
@@ -34,33 +34,30 @@ for hısse in symbol:
 
    cand=f"https://finnhub.io/api/v1/stock/candle?symbol={hısse}&resolution=D&count=30&token={api_key}"
    can=requests.get(cand).json()
-   if can.get("s") == "ok":
-      
-      dff=pd.DataFrame({
-         "kapanış":can["c"],
-         "tarih":pd.to_datetime(can["t"], unit="s")
-            })
-      dff["getiri"]=dff.pct_change()
-      ortalama_getiri=dff["getiri"].mean()
-      standrart_sapma=dff["getiri"].std()
-      risk=0.00
-      sharp=(ortalama_getiri - risk)/ standrart_sapma
-    
-   else:
-      sharp=None
+  
    
    al=requests.get(url)
    data=al.json()
    data["yüzde_değişim"]=((data["c"] - data["o"] )/ data["o"] )*100
+
+   #Tahmini hisse fiyatı
+   mm=url_zmn.get("marketCapitalization") / url_zmn.get("shareOutstanding")
+   # açılış kapanış fiyatı arasındaki far
+   akff=data["c"] - data["o"]
+
+   # low hight fiyat farkı
+   lhff=data["h"] -data["l"]
   
    bra.append({
       "Hisse":hısse,
-      "kapanış":data["c"],
+      "kapanış":data["c"],# kapanış fiyatı
       "hight": data["h"],
       "low": data["l"],
-      "açılış":data["o"],
+      "açılış":data["o"],# açılış fiyatı
       "değişim":data["yüzde_değişim"],
-      "sharp":sharp, 
+      "açılış_kapanış_fiyat_farkı":akff,
+      "fiyat_aralığı_yüksek_düşük":lhff,
+      "Fiyat_tahmini":mm ,
       "tarih": pd.to_datetime(data["t"], unit="s"),
       "Ülke":url_zmn.get("country"),
       "para_birimi":url_zmn.get("currency"),
@@ -73,9 +70,22 @@ for hısse in symbol:
       "kazanç_raporu":ear,
       "rakipler": rakip,
     })
+   
+"""   
+print(data.columns)  
+print(data.head())
+print(data.tail())
+print(data.info())
+print(data.dtypes)
+print(data.describe())
+print(data.isnull().sum())
+print(data.shape)
+print(data.memory_usage())
+print(data.index)
+print(data.nunique())
+"""
 brahs=pd.DataFrame(bra)
 
 brahs.to_excel("C:\\Users\\buğra\\Desktop\\abd_borsaa_mydata.xlsx")
-
 
 
